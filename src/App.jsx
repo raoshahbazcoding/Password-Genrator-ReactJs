79b7216951478c646e9216792806ecb630c9cba0
Rao Shahbaz IMAC <meracodestyle@gmail.com>
2025-09-15 01:52:48 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+<>
+<h1 className='text-center bg-blue-400 font-extrabold py-4 text-white text-4xl'>
+  Password Genrator
+</h1>
+<div className='h-[93vh] bg-black'>
+
+
+
+  
+</div>
+
+</>
+  )
+}
+
+export default App
